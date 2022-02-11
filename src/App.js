daf9f5dab8887ef0ec7b1c65670165b5f5ffe68d
Z-Xiaobi <935213752@qq.com>
2022-02-11 16:59:05 -0500

    Map data into component Card

--- src/App.js
+++ src/App.js
@@ -2,22 +2,32 @@ import './App.css';
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Card from './components/Card'
+import data from './data'
 
 function App() {
-  return (
-    <div>
-      <Navbar />
-      <Hero />
-      <Card
-        img="katie-zaferes.png"
-        rating="5.0"
-        reviewCount={6}
-        country="USA"
-        title="Life Lessons with Katie Zaferes"
-        price={136}
-       />
-    </div>
-  );
-}
+    const cards = data.map(item => {
+        return (
+            <Card
+                img={item.coverImg}
+                rating={item.stats.rating}
+                reviewCount={item.stats.reviewCount}
+                location={item.location}
+                title={item.title}
+                price={item.price}
+            />
+        )
+    })
+
+
+    return (
+        <div>
+            <Navbar />
+            <Hero />
+            { cards }
+        </div>
+    );
+ }
 
 export default App;
+
+
