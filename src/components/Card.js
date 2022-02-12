12b533220e46e0ac7a8a3bb1b524e6b8279776dc
Z-Xiaobi <935213752@qq.com>
2022-02-12 17:54:39 -0500

    make card attributes precise in App.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -11,9 +11,9 @@ export default function Card(props) {
     }
 
     let badgeText
-    if (props.openSpots === 0) {
+    if (props.item.openSpots === 0) {
         badgeText = "SOLD OUT"
-    } else if (props.location === "Online") {
+    } else if (props.item.location === "Online") {
         badgeText = "ONLINE"
     }
     // JS
@@ -22,19 +22,19 @@ export default function Card(props) {
         <div className="card">
 
             {/* use the truthness of variable with values and falseness of undefined*/}
-            { badgeText && <div className="card--badge">SOLD OUT</div>}
+            { badgeText && <div className="card--badge"> {badgeText} </div>}
 
             {/* src={} back to JS, then at 'className' back to JSX */}
             {/* <img src={`../images/${props.img}`} className="card--image" />*/}
-            <img src={ IMGPATH(props.img) } className="card--image" alt=""/>
+            <img src={ IMGPATH(props.item.coverImg) } className="card--image" alt=""/>
             <div className="card--stats">
                 <img src={ RedStar} className="card--star" alt="rate star"/>
-                <span>{ props.rating }</span>
-                <span className="gray">({ props.reviewCount }) • </span>
-                <span className="gray">USA</span>
+                <span>{props.item.stats.rating}</span>
+                <span className="gray">({props.item.stats.reviewCount}) • </span>
+                <span className="gray">{props.item.location}</span>
             </div>
-            <p> {props.title} </p>
-            <p><span className="bold">From ${ props.price }</span> / person</p>
+            <p className="card--title"> {props.item.title} </p>
+            <p><span className="bold">From ${ props.item.price }</span> / person</p>
         </div>
     )
 }
